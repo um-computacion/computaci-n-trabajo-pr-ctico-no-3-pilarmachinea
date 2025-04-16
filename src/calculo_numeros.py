fd09e8ca5da2dc7e0d6c9686c726e3345faf3b7c
pilarmachinea <m.machinea@alumno.um.edu.ar>
2025-04-16 12:54:01 -0300

    hubieron cambios

--- src/calculo_numeros.py
+++ src/calculo_numeros.py
@@ -3,7 +3,6 @@ from mis_exceptions import ingrese_numero, NumeroDebeSerPositivo
 def sumar_dos_numeros():
     num1 = ingrese_numero("Ingrese el primer número: ")
     num2 = ingrese_numero("Ingrese el segundo número: ")
-
     resultado = num1 + num2
     print(f"La suma de los números es: {resultado}")
 
